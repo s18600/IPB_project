714d93798a95be7d618935c914b657a33feb2f9a
Oskar <lol199pl@gmail.com>
2021-06-14 20:50:22 +0200

    init login, cashier dashboard, customer dashboard, sign in, new order form

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,16 +1,14 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -22,9 +20,17 @@ public class Main extends Application {
         launch(args);
     }
 
+    public static final ObservableList data =
+            FXCollections.observableArrayList();
+    Stage dashboardStage;
+    Stage primaryStage;
+    boolean isCashier;
+    String name;
+
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("JavaFX Welcome");
+    public void start(Stage ps) {
+        primaryStage = ps;
+        primaryStage.setTitle("Garden store app");
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
@@ -35,7 +41,7 @@ public class Main extends Application {
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         grid.add(scenetitle, 0, 0, 2, 1);
 
-        Label userName = new Label("User Name:");
+        Label userName = new Label("Email:");
         grid.add(userName, 0, 1);
 
         TextField userTextField = new TextField();
@@ -47,18 +53,31 @@ public class Main extends Application {
         PasswordField pwBox = new PasswordField();
         grid.add(pwBox, 1, 2);
 
-        Button btn = new Button("Sign in");
+        Button logInBtn = new Button("Log in");
+        Button signInBtn = new Button("Sign in");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
+        hbBtn.getChildren().add(logInBtn);
+        hbBtn.getChildren().add(signInBtn);
         grid.add(hbBtn, 1, 4);
+
 
         final Text actiontarget = new Text();
         grid.add(actiontarget, 1, 6);
 
-        btn.setOnAction(e -> {
-            actiontarget.setFill(Color.FIREBRICK);
-            actiontarget.setText("Sign in button pressed");
+        logInBtn.setOnAction(e -> {
+            String[] email = userTextField.getText().split("@");
+            if (email[email.length - 1].equals("garden.com")) {
+                name = email[0];
+                cashierDashboard();
+            } else {
+                name = email[0];
+                customerDashboard();
+            }
+        });
+
+        signInBtn.setOnAction(event -> {
+            signIn();
         });
 
 
@@ -66,4 +85,185 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    private void cashierDashboard() {
+        isCashier = true;
+        dashboardStage = new Stage();
+        dashboardStage.initOwner(primaryStage);
+        dashboardStage.setTitle("Cashier id:  " + name);
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Cashier account: " + name);
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Button logOut = new Button("Log out");
+        Button ordersToAccept = new Button("Orders to accept");
+        Button insertStationaryOrder = new Button("New stationary order");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().addAll(logOut, ordersToAccept, insertStationaryOrder);
+        grid.add(hbBtn, 1, 4);
+
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        logOut.setOnAction(e -> {
+            start(primaryStage);
+        });
+
+        insertStationaryOrder.setOnAction(event -> {
+            newOrder();
+        });
+
+
+        Scene scene = new Scene(grid, 600, 480);
+        primaryStage.setScene(scene);
+    }
+
+    private void customerDashboard() {
+        isCashier = false;
+        dashboardStage = new Stage();
+        dashboardStage.initOwner(primaryStage);
+        dashboardStage.setTitle("Cashier id:  " + name);
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome " + name);
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Button log_out = new Button("Log out");
+        Button ordersToAccept = new Button("Check orders details");
+        Button insertStationaryOrder = new Button("New order");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().addAll(log_out, ordersToAccept, insertStationaryOrder);
+        grid.add(hbBtn, 1, 4);
+
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        log_out.setOnAction(e -> {
+            start(primaryStage);
+        });
+
+
+        Scene scene = new Scene(grid, 600, 480);
+        primaryStage.setScene(scene);
+    }
+
+    private void signIn() {
+        dashboardStage = new Stage();
+        dashboardStage.initOwner(primaryStage);
+        dashboardStage.setTitle("Cashier id:  ");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome ");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("Email:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Label pw2 = new Label("Confirme password:");
+        grid.add(pw2, 0, 3);
+
+        PasswordField pwBox2 = new PasswordField();
+        grid.add(pwBox2, 1, 3);
+
+        Button sign_in = new Button("Sign in");
+        Button back = new Button("Back");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().addAll(back, sign_in);
+        grid.add(hbBtn, 1, 4);
+
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        back.setOnAction(e -> {
+            start(primaryStage);
+        });
+
+        sign_in.setOnAction(e -> {
+            start(primaryStage);
+        });
+
+
+        Scene scene = new Scene(grid, 600, 480);
+        primaryStage.setScene(scene);
+    }
+
+    private void newOrder() {
+        dashboardStage = new Stage();
+        dashboardStage.initOwner(primaryStage);
+        dashboardStage.setTitle("Cashier id:  ");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        final ListView listView = new ListView(data);
+        listView.setPrefSize(200, 250);
+        listView.setEditable(true);
+
+        listView.setItems(data);
+
+        grid.getChildren().add(listView);
+
+        Label desc = new Label("Add positions:");
+        grid.add(desc, 0, 1);
+
+        TextField addText = new TextField();
+        grid.add(addText, 0, 2);
+
+        Button add = new Button("Add");
+        grid.add(add, 0, 3);
+
+        add.setOnAction(event -> {
+            if (!addText.getText().isEmpty()) {
+                data.add(addText.getText());
+                addText.clear();
+            }
+        });
+
+        Button submit = new Button("Submit");
+        grid.add(submit, 1, 3);
+
+        submit.setOnAction(event -> {
+            if (isCashier) {
+                cashierDashboard();
+            }
+        });
+
+
+        Scene scene = new Scene(grid, 600, 480);
+        primaryStage.setScene(scene);
+    }
+
 }
